ad37a7ed8dbddf1edaf0c482989f1f387272df71
Matt <mupham1@gmail.com>
2022-06-12 19:41:28 -0600

    styles and js

--- src/scripts.js
+++ src/scripts.js
@@ -10,15 +10,33 @@ function onLoad() {
 
 const url = "https://swcodegenerator.azurewebsites.net/api/GetCode";
 code = "";
-fetch(url)
-  .then(
-    function(response) {return response.json()} // .json(), .blob(), etc.
-  ).then(
-    function(json) {console.log(json)
+// fetch(url)
+//   .then(
+//     function(response) {return response.json()} // .json(), .blob(), etc.
+//   ).then(
+//     function(json) {console.log(json)
         
-        document.getElementById('code').innerHTML = json.code
-    } // Handle here
-  )
+//         document.getElementById('code').innerHTML = json.code
+//     } // Handle here
+//   )
+
+document.getElementById('code').innerHTML = getCode()
+}
+
+
+function getCode()
+{
+  var now = new Date();
+  var start = new Date(now.getFullYear(), 0, 0);
+  var diff = now - start;
+  var oneDay = 1000 * 60 * 60 * 24;
+  var day = Math.floor(diff / oneDay);
+  console.log('Day of year: ' + day);
+  var code = 800
+  var randomnum = Math.floor(Math.random() * (80000 - 20000 + 1) + 20000)
+
+  return "" + day + code + randomnum
+
 }
 
 window.onload = onLoad();
