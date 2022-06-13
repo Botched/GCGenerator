662f3a808d2854de1e0e2b4fc8f0fd81e4712972
Matt <mupham1@gmail.com>
2022-06-12 18:39:58 -0600

    JS

--- src/scripts.js
+++ src/scripts.js
@@ -1,11 +1,22 @@
 function onLoad() {
-    var request = new XMLHttpRequest()
+//     var request = new XMLHttpRequest()
 
-request.open('POST', 'https://swcodegenerator.azurewebsites.net/api/GetCode', true)
+// request.open('POST', 'https://swcodegenerator.azurewebsites.net/api/GetCode', true)
 
-request.onload = function() { console.log(data) }
+// request.onload = function() { console.log(data) }
 
-request.send()
+// request.send()
+
+
+const url = "https://swcodegenerator.azurewebsites.net/api/GetCode";
+fetch(url)
+  .then(
+    response => response.text() // .json(), .blob(), etc.
+  ).then(
+    text => console.log(text) // Handle here
+  );
+
+
 }
 
 window.onload = onLoad();
