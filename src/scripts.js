f3409192c4ef6a176c9abeea8c32357364090fbb
Matt <mupham1@gmail.com>
2022-06-12 19:53:30 -0600

    styles and js

--- src/scripts.js
+++ src/scripts.js
@@ -8,8 +8,8 @@ function getCode()
   console.log('Day of year: ' + day);
   var code = 800
   var randomnum = Math.floor(Math.random() * (80000 - 20000 + 1) + 20000)
-
-  document.getElementById('code').innerHTML = "" + day + code + randomnum
+  var valueToSet = "" + day + code + randomnum;
+  document.getElementById('code').innerHTML = valueToSet;
 
 }
 
